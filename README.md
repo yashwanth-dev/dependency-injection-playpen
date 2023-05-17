d217b9afd0c41bae15d4d4ba2d120657b69d541b
yashwanth-dev <yashwanthp.developer@github.com>
2023-05-17 20:24:17 +0530

    [Step-3] Picking up writer from the conmfiguration

--- README.md
+++ README.md
@@ -1,3 +1,4 @@
 # Dependency Injection
 1. Print Hello world in console
 2. I should be able to pick the writer dynamically
+3. I want to pick the writer from configuration file.
--- src/DI.Console/Program.cs
+++ src/DI.Console/Program.cs
@@ -1,12 +1,23 @@
 ﻿using DI.Console;
+using Microsoft.Extensions.Configuration;
 
 public class Program
 {
     static void Main(string[] args)
     {
         string message = "Hello DI!";
-        IMessageWriter writer = new ConsoleWriter();
+        IMessageWriter writer = GetMessageWriter();
         var salutation = new Salutation(writer);
         salutation.Exclaim(message);
+    }
+
+    static IMessageWriter GetMessageWriter()
+    {
+        IConfigurationRoot configuration = new ConfigurationBuilder().SetBasePath(Directory.GetCurrentDirectory())
+            .AddJsonFile("appsettings.json").Build();
+        string typeName = configuration["messageWriter"];
+        Type type = Type.GetType(typeName, throwOnError: true);
+        IMessageWriter writer = (IMessageWriter) Activator.CreateInstance(type);
+        return writer;
     }
 }
